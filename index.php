05494be39a6d551309cb2f7639cea9239345d94c
PatrickkBS <patrick051198@gmail.com>
2021-08-24 23:18:06 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html  lang = " en ">
+
+< cabeça >
+    <meta  charset = " utf-8 " >
+    <meta  name = " viewport "  content = " width = device-width, initial-scale = 1 " >
+    <título > AdminLTE 3 | Entrar (v2) </ title >
+    <! - Fonte do Google: Source Sans Pro ->
+    <link  rel = " stylesheet "  href = " https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback " >
+    <! - Fonte incrível ->
+    <link  rel = " stylesheet "  href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.scs" >
+    <! - icheck bootstrap ->
+    <link  rel = " stylesheet "  href = "https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css " >
+    <! - Estilo do tema ->
+    <link  rel = " stylesheet "  href = "https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js" >
+</head >
+<body  class = " hold-transaction login-page " >
+    <div  class = " login-box " >
+        <! - /.login-logo ->
+        <div  class = " cartão card-contorno card-primary " >
+            <div  class = " card-header text-center " >
+                <A  href = " ../../index2.html "  class = " h1 " > < b > Administrador </ b > LTE </ a >
+            </div >
+            <div  class = " card-body " >
+                <div  class = " callout callout-warning " >
+                    <h5 > Atenção! </ h5 >
+                    <p > Todos os campos sinalizados com ** são obrigatórios. </ p >
+                </div >
+                <form  action = " ../../index3.html "  method = " post " >
+                    <div  class = " input-group mb-3 " >
+                        <input  type = " email "  class = " form-control "  placeholder = " Digite seu Celular ou E-mail * " >
+                        <div  class = " input-group-append " >
+                            <div  class = " input-group-text " >
+                                <span  class = " fas fa-envelope " > </ span >
+                            </div >
+                        </div >
+                    </div >
+                    <div  class = " input-group mb-3 " >
+                        <input  type = " password "  class = " form-control "  placeholder = " Digite sua senha * " >
+                        <div  class = " input-group-append " >
+                            <div  class = " input-group-text " >
+                                <span  class = " fas fa-lock " > </ span >
+                            </div >
+                        </div >
+                    </div >
+                    <div  class = " row " >
+                        
+                        <! - /.col ->
+                        <div  class = " col-12 " >
+                            <button  type = " submit "  class = " btn btn-primary btn-block " > Entrar </ button >
+                        </div >
+                        <!- /.col ->
+                    </div >
+                </form >
+                <p  class = " mb-2 " >
+                    <A  href = " register.html "  class = " text-center " > registrar uma nova associação </ a >
+                </p >
+                <p  class = " mb-0 " >
+                    <A  href = " register.html "  class = " text-center " > registrar uma nova associação </ a >
+                </p >
+            </div >
+            <! - /.card-body ->
+        </div >
+        <!- /.card ->
+    </div >
+    <! - /.login-box ->
+    <! - jQuery ->
+    <script  src = " templete / AdminLTE-3.1.0 / plugins / jquery / jquery.min.js " > </ script >
+    <! - Bootstrap 4 ->
+    <script  src = " templete / AdminLTE-3.1.0 / plugins / jquery / bootstrap.bundle.min.js " > </ script >
+    <! - Aplicativo AdminLTE ->
+    < script  src = " templete / AdminLTE-3.1.0 / plugins / jquery / adminlte.min.js " > </ script >
+</ body >
+</html> 
